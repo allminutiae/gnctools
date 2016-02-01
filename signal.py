d285e8a1f1567b21d2530c4d90653e0b123faefb
kgeohaga <kevin.geohagan@nasa.gov>
2016-02-01 15:04:26 -0600

    Added sinusoid() function to produce a discrete-sampled sinusoid with given signal and sample frequency (and phase shift and time).

--- signal.py
+++ signal.py
@@ -31,3 +31,28 @@ def psd(data:  'a 1xn (or nx1) numpy.ndarray', # the data to be asd'ed
     freqs  = np.fft.rfftfreq(targetlen, d=1./fsamp)
 
     return freqs, psd_db
+
+def sinusoid(f_sig:     float,
+             f_samp:    float,
+             t_end:     float,
+             shift_rad: float=0):
+    """
+    Creates a discrete-sampled sinusoid and accompanying array of sample times.
+
+    :param f_sig:       signal frequency
+    :param f_samp:      sample frequency
+    :param t_end:       end time
+    :param shift_rad:   phase shift applied
+
+    :return: t:         iterable (numpy array) of time data points
+             sig:       iterable (numpy array) of signal corresponding to t
+
+    TODO: make a little less explicitly coupled to producing time-based data
+    """
+    n_sig  = t_end*f_sig      # number of cycles
+    n_samp = t_end*f_samp + 1 # number of samples
+    th  = np.linspace(shift_rad, n_sig*2*np.pi + shift_rad, n_samp)
+    t   = np.linspace(0, t_end, n_samp)
+    sig = np.sin(th)
+
+    return t, sig
